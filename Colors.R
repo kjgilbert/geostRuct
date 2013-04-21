98769851fcd4310c1163a1bfaf765d453ebc3a7c
kjgilbert <kgilbert@zoology.ubc.ca>
2013-04-21 15:51:14 -0700

    these will probably need some adding to

---
+++ Colors.R
@@ -0,0 +1,13 @@
+
+#default color palettes
+
+
+
+
+
+#these currently set up to work with bar plots, can probably be made to use with pie charts
+colors <- c("khaki", "darkblue", "steelblue2", "darkorange", "green4", "red2", "purple", "yellow")
+	#can add more colors if k>8, or can alter defaults as desired
+bw <- c("black", "gray95", "gray50", "gray25", "gray75", "gray40", "gray85")
+	#black and white color palette option
+
