4a522daec4e837d8f8f28f6c6ea2d37538535980
kjgilbert <kgilbert@zoology.ubc.ca>
2013-04-21 14:55:41 -0700

    stuck :( how to make data formatting a function

--- MakeBarPlots.R
+++ MakeBarPlots.R
@@ -11,47 +11,61 @@
 #}
 #windows(width=6, height=6)
 
-## Set working directory
-#setwd("../colbeck.locprior0.AdmCorr10k100k/")   # file path of STRUCTURE results
+
 ## Select file
-qfile="~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/GlobalData 2/GlobalData2/Results/GlobalData2_run_4_f"
+qfile <- "~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/GlobalData 2/GlobalData2/Results/GlobalData2_run_4_f"
 
-qfile="~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/Results/GlobalData_WithSAMS1_11092010_run_5_f"
+qfile <- "~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/Results/GlobalData_WithSAMS1_11092010_run_5_f"
+
+qfile <- "~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/Results/GlobalData_WithSAMS1_11092010_run_4_f"
+qfile <- "~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/Results/GlobalData_WithSAMS1_11092010_run_6_f"
+
+qfile <- "~/Documents/My_Documents/Taylor_Lab/Data/My Data/Structure/Results/GlobalData_WithSAMS1_11092010_run_3_f"
+
+read.q <- function(qfile){
+	## Read in Q and identify K
+	qstart <- which(read.table(qfile,sep="\t",blank.lines.skip =F, stringsAsFactors=F)=="Inferred ancestry of individuals:")+1
+	qend <- which(read.table(qfile,sep="\t",blank.lines.skip =F, stringsAsFactors=F)=="Estimated Allele Frequencies in each cluster")-3
+	nind <- qend-qstart
+	qall0 <- read.table(qfile, skip=qstart, nrows=nind, stringsAsFactors = FALSE, strip.white = TRUE, header=FALSE, na.strings = c("NA","",".","#DIV/0!","#REF!","#VALUE!","#NUM!"))
+	qdatstr <- ":"
+	selectedCols <- qall0[, grep(qdatstr, qall0)]  #find column with colons, after this column is the columns where the Q values are stored
+	rawcolumn <- tail(names(selectedCols),1)
+	column <- which(colnames(qall0)==rawcolumn)  #this is the column containing colons, add 1 to it in the next step for starting q values
+	firstcol <- column+1 ## first column of Q estimates- this may need to be altereddepending on location data and/or labels included in the analysis
+	k <- sum(sapply(firstcol:ncol(qall0),function(x){is.numeric(qall0[,x])}))
+	## count assumed populations
+
+	## Set up population sizes for labels
+	qall <- qall0[order(qall0[,3]),]   ## you can sort them by population here (optional)
+	qall$pops <- as.factor(qall[,3])
+	popsizes0 <- table(qall$pops)
+	popsizes <- popsizes0[order(match(levels(qall$pops),qall$pops))]
+
+	#make matrix of q values for plotting
+	return(bars <- t(as.matrix(qall[,(firstcol-1)+1:k])) ) #return transpose of matrix
+	#return(bars)
+}
 
 
+# Barplots ala Structure/distruct
+bars <- t(as.matrix(qall[,(firstcol-1)+1:k]))  #return transpose of matrix
+#resize.win(8,3)
+
+colors <- c("khaki", "darkblue", "steelblue2", "darkorange", "green4", "red2", "purple", "yellow")  ## Add more colors if K>6 and change these if you like
+bw <- c("black", "gray95", "gray50", "gray25", "gray75", "gray40", "gray85")
+
+#par(fig=c(0,1,0,1),mar=c(2,2,1,1),oma=c(1,0.5,0.5,0.5),xaxs="i",yaxs="i",bg=NA)
 
 
-## Read in Q and identify K
-qstart=which(read.table(qfile,sep="\t",blank.lines.skip =F, stringsAsFactors=F)=="Inferred ancestry of individuals:")+1
-qend=which(read.table(qfile,sep="\t",blank.lines.skip =F, stringsAsFactors=F)=="Estimated Allele Frequencies in each cluster")-3
-nind=qend-qstart
-qall0<-read.table(qfile, skip=qstart, nrows=nind, stringsAsFactors = FALSE, strip.white = TRUE, header=FALSE, na.strings = c("NA","",".","#DIV/0!","#REF!","#VALUE!","#NUM!"))
-head(qall0)
-firstcol=6 ## first column of Q estimates- this may need to be altereddepending on location data and/or labels included in the analysis
-k=sum(sapply(firstcol:ncol(qall0),function(x){is.numeric(qall0[,x])}))
-## count assumed populations
+plot.bars <- function(bars, wid=1, sp=0, title="", bord=NA, color=colors)
+{
+	par(fig=c(0,1,0,1),mar=c(2,2,1,1),oma=c(1,0.5,0.5,0.5),xaxs="i",yaxs="i",bg=NA)
+	barplot(bars, width=wid, space=sp, names.arg=rep("",ncol(bars)), col=color, border=bord, main=title) #having things like "border=border" creates a recursive error
+	par(lwd=1)
+	par(new=T)
+	barplot(popsizes/popsizes, width=popsizes, space=sp, col=NA)
+}
 
-## Set up population sizes for labels
-qall=qall0[order(qall0[,3]),]   ## you can sort them by population here (optional)
-qall$pops=as.factor(qall[,3])
-popsizes0=table(qall$pops)
-popsizes=popsizes0[order(match(levels(qall$pops),qall$pops))]
 
-# Stacked Bar Plot with Colours
-bars=t(as.matrix(qall[,(firstcol-1)+1:k]))
-resize.win(8,3)
-colours=c("blue","green","red","yellow","pink","purple")  ## Add more colours if K>6 and change these if you like
-par(fig=c(0,1,0,1),mar=c(2,2,1,1),oma=c(1,0.5,0.5,0.5),xaxs="i",yaxs="i",bg=NA)
-barplot(bars, width = 1, space = 0, names.arg = rep("",ncol(bars)), legend.text = NULL, beside = FALSE,
-        horiz = FALSE, density = NULL, angle = 45,
-        col = colours, border = NA,
-        main = NULL, sub = NULL, xlab = NULL, ylab = NULL,
-        xlim = NULL, ylim = NULL, xpd = TRUE, log = "",
-        axes = TRUE, axisnames = TRUE,
-        cex.axis = par("cex.axis"), cex.names = par("cex.axis"),
-#        inside = TRUE, plot = TRUE, axis.lty = 0, offset = 0,
-        add = FALSE, args.legend = NULL)
-par(lwd=1)
-par(new=T)
-barplot(popsizes/popsizes,width=popsizes,space=0,col=NA)
 #savePlot(filename = paste(qfile,".png",sep=""), type = "png",device = dev.cur(), restoreConsole = TRUE)
